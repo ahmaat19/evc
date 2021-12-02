1b2af6bd3e612762c68caac2627bd1c0ad817e0e
Ahmad Ibrahim <ahmaat19@gmail.com>
2021-12-02 15:47:58 +0300

    adding api calls

---
+++ api/dynamicApi.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import { config } from '../utils/customLocalStorage'
+
+const dynamicAPI = async (method, url, obj = {}) => {
+  try {
+    switch (method) {
+      case 'get':
+        return await axios.get(`${url}`, config()).then((res) => res.data)
+
+      case 'post':
+        return await axios.post(`${url}`, obj, config()).then((res) => res.data)
+
+      case 'put':
+        return await axios.put(url, obj, config()).then((res) => res.data)
+
+      case 'delete':
+        return await axios.delete(url, config()).then((res) => res.data)
+    }
+  } catch (error) {
+    throw error.response.data
+  }
+}
+
+export default dynamicAPI
+
+// export const config = () => {
+//     return {
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization:
+//           customLocalStorage() &&
+//           customLocalStorage().userInfo &&
+//           `Bearer ${customLocalStorage().userInfo.token}`,
+//       },
+//     }
+//   }
